0f5052a0ccdfdefd689a47366ea6faca62ef4bd9
Nathan Givens <nate.givens03@gmail.com>
2020-03-23 17:51:01 -0400

    create verification url

--- lib.mjs
+++ lib.mjs
@@ -1,8 +1,8 @@
-// import config from "./config.js";
-import mask from "@trellisfw/masklink";
+import config from "./config.js";
+import ml from "@trellisfw/masklink";
 
 // const TRELLIS_URL = "localhost";
-// const TRELLIS_URL = config.get("trellis_url");
+const TRELLIS_URL = config.get("trellis_url");
 
 import TEST_AUDIT from "./auditMasked.js";
 
@@ -27,7 +27,7 @@ export class VDoc {
 
   constructor(vDoc) {
     this.vDoc = vDoc;
-    this.masks = mask.findAllMaskPathsInResource(this.vDoc);
+    this.masks = ml.findAllMaskPathsInResource(this.vDoc);
   }
 
   access(path) {
@@ -38,7 +38,7 @@ export class VDoc {
     let obj = this.vDoc;
     for (const step in steps) {
       if (obj[steps[step]]) {
-        if (!mask.isMask(obj)) {
+        if (!ml.isMask(obj)) {
           obj = obj[steps[step]];
         } else {
           return {
@@ -74,8 +74,8 @@ export class VDoc {
 
   getDisplayLocation(path) {
     const { res } = this.access(path);
-    if (mask.isMask(res)) {
-      return getVerificationURL(res);
+    if (ml.isMask(res)) {
+      return getVerificationQuery(res);
     }
     return `${res.street_address},
         ${res.city},
@@ -87,8 +87,8 @@ export class VDoc {
   getDisplayScheme() {
     const { res } = this.access("/scheme");
     // not sure why this would ever happen...
-    if (mask.isMask(res)) {
-      return getVerificationURL(res);
+    if (ml.isMask(res)) {
+      return getVerificationQuery(res);
     }
     if (res.name === "SQFI") {
       if (res.edition === "8.0") {
@@ -101,14 +101,31 @@ export class VDoc {
   }
 }
 
+function getVerificationQuery(mask) {
+  if (!mask) {
+    return "";
+  }
+  if (mask["trellis-mask"]) {
+    mask = mask["trellis-mask"];
+  }
+  return `${TRELLIS_URL}/verify?${Object.keys(mask)
+    .filter(key => typeof mask[key] !== "object")
+    .map(key => {
+      return `${encodeURIComponent(key)}=${mask[key]}`;
+    })
+    .join("&")}&${encodeURIComponent("hashinfo.alg")}=${
+    mask["hashinfo"]["alg"]
+  }&${encodeURIComponent("hashinfo.hash")}=${mask["hashinfo"]["hash"]}`;
+}
+
 (() => {
   const v1 = new VDoc(TEST_AUDIT);
-  console.log(v1.masks);
   if (v1.masks) {
     v1.masks.forEach(mask => {
       console.log(mask);
-      console.log(v1.access(mask));
+      console.log(v1.getDisplayLocation(mask));
+      // console.log(getVerificationQuery(mask));
     });
   }
-  console.log(v1.access("one1/three/four1"));
+  // console.log(v1.access("one1/three/four1"));
 })();
